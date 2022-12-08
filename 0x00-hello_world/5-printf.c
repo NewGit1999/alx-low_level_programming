bce8ad62d646e28c97c89ee8e604c88b9d4bdaf2
Your Name <you@example.com>
2022-12-08 12:04:22 -0800

    commit 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - The entry point 
+ * main - Entry point 
  * Return: Always 0.
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
