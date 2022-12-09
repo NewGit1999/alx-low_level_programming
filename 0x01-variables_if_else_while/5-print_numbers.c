0a09ae2e045633cb4066a36821a10b6e64fd317a
Your Name <you@example.com>
2022-12-09 15:44:59 -0800

    block 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int nb;
+	
+	for (nb = 0; nb < 10; nb++)
+		printf("%d", nb);
+	printf("\n");
+	return (0);
+}
