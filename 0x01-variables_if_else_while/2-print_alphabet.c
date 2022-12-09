6a81b5cfc40c6aca0bc78db0aa8857c79b3a13c0
Your Name <you@example.com>
2022-12-09 14:12:23 -0800

    block 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter; letter<=; letter++)
+		putchar(letter);
+	putchar ('\n');
+	return (0);
+}
+
+
