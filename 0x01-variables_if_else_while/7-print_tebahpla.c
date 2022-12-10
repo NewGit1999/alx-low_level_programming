797708ed759adedc8fbe24a71bcf450c295ba396
Your Name <you@example.com>
2022-12-09 16:12:34 -0800

    block 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio>
+/**
+ * main - Entry point
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char let;
+
+	for (let = 'z'; let >= 'a'; let++)
+		putchar(let);
+	putchar('\n');
+	return (0);
+}
