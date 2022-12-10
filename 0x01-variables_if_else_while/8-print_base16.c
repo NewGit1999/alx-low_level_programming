5991bda7b24f73146f350df6926c27fa7cb753a9
Your Name <you@example.com>
2022-12-09 16:43:14 -0800

    block 6

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,10 @@ int main(void)
 	int nb;
 	char let;
 
-	for (let = 'a'; let <= 'f'; let++)
-		putchar(let);
 	for (nb = 0; nb < 10; nb++)
 		putchar((nb % 10) + '0');
+	for (nb = 'a'; let <= 'f'; let++)
+		putchar(let);
 	putchar('\n');
 	return (0);
 }
