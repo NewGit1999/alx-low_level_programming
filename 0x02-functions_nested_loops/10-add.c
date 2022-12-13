d6587555bf39f25d1cb61115cd1a13c79602797a
Your Name <you@example.com>
2022-12-13 14:51:07 -0800

    function 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - result of addition
+ * @a: first number.
+ * @b: second number.
+ * Return: result of addition of two numbers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
