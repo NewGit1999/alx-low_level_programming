db9df3093b25ea89cadd1cb23af86a0c69e6937e
Your Name <you@example.com>
2022-12-13 15:14:24 -0800

    function 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_to_98 - print all the numbers from first to 98.
+ * @n: the number to begin with .
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
