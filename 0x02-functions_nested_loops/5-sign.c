725b94b67b596e18936cf796301760a07bc74fca
Your Name <you@example.com>
2022-12-13 11:58:07 -0800

    function 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - check the sign of the number
+ * @n: the number to print its sign
+ * Return: 1 if the number is greater than zero, 0 if tne number is equal to zero, -1 if the number is less than zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
