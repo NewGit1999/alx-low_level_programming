0957ff38841b499f13cb04613af4a5ce24010673
Your Name <you@example.com>
2022-12-14 13:54:49 -0800

    debug 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,12 +3,9 @@
 
 
 /**
- *
- * * main - tests function that prints if integer is positive or negative
- *
- * * Return: 0
- *
- * */
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 
 
