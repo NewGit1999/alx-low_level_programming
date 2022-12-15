348d35b56639b0f60c30c2dcada4eee00f49cc3d
Your Name <you@example.com>
2022-12-15 15:49:42 -0800

    prog 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - product of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
