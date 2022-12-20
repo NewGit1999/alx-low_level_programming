3035eef8247997901f54e862d512f159cb817766
Your Name <your email>
2022-12-19 18:17:31 -0800

    point 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - prints the half of string
+ * @str: string asked
+ */
+void puts_half(char *str)
+{
+	int index = 0, length = 0, m;
+
+	while (str[index])
+		length++;
+
+	if ((length % 2) == 0)
+		m = length / 2;
+	else
+		m = (length + 1) / 2;
+	for (index = m; index < length; index++)
+		_putchar(str[index]);
+	_putchar('\n');
+}
