014cf5b7a1d2e7fb1872c08a19f39186f9d96fe1
Your Name <your email>
2022-12-19 17:15:14 -0800

    point 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,9 +7,9 @@ void print_rev(char *s)
 {
 	int in;
 
-	for (in = 0; s[i] != '0'; in++)
+	for (in = 0; s[i] != '\0'; in++)
 
-		for (in = in - 1; s[in] != '0'; in--)
+		for (in = in - 1; s[in] != '\0'; in--)
 			_putchar(s[in]);
 	_putchar('\n');
 }
