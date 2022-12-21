63687c6050a1e5443a3697b660c772fe809f8c5e
Your Name <your email>
2022-12-21 08:24:30 -0800

    value 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcat - concates two strings
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: the pointer resulted
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	for (i = 0; src[i]; i++)
+		dest[j++] = src[i];
+	return (dest);
+}
