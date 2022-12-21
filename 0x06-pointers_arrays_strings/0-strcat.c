7b600cef0e62c336f092e183a101528174d18293
Your Name <your email>
2022-12-21 09:18:38 -0800

    value 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,12 +8,21 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
+	int i;
+	int j;
 
-	while (dest[i]++)
+	i = 0;
+	while (dest[i] != '\0')
+	{
 		i++;
-	for (i = 0; src[i]; i++)
-		dest[j++] = src[i];
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
