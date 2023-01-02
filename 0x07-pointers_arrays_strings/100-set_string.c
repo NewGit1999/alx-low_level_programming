7ea2b1fdf639d2df08dc615175ff1da13fe441f6
Your Name <your email>
2023-01-02 05:14:19 -0800

    more 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to set
+ * @to: replacer
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
