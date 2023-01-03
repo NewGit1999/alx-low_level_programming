fbdbff13ff65c2c5367b27d53f9b2232386a3e5b
Your Name <your email>
2023-01-03 09:34:04 -0800

    more 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,7 +15,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		a = haystack;
 		b = needle;
-		while (*b == haystack && *b != '\0' && *haystack != '\0')
+		while (*b == *haystack && *b != '\0' && *haystack != '\0')
 		{
 			haystack++;
 			b++;
