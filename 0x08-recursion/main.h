97af20c07c80ad30b830863a3651a46bce875b14
Your Name <your email>
2023-01-04 10:51:55 -0800

    my header

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -13,6 +13,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int getSqr(int a, int b);
 int is_prime_number(int n);
+int getPrime(int a, int b);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
