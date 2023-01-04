3788bd9a34f411183f929d15ea822046e35a6b65
Your Name <your email>
2023-01-04 09:59:41 -0800

    recursion 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,5 +12,21 @@ int _sqrt_recursion(int n)
 		return (1);
 	if (n < 0)
 		return (-1);
-	return (_sqrt_recursion(n, 1));
+	return (getSqr(n, 1));
+}
+
+/**
+ * getSqr - gets the number of square root
+ * @a: input
+ * @b: count
+ * Return: int
+ */
+int getSqr(int a, int b)
+{
+	if (a == (b * b))
+		return (b);
+	else if (a > (b * b))
+		return (getSqr(a, b + 1));
+	else
+		return (-1);
 }
