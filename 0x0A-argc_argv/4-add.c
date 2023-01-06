54e31613eb31eb35214aa7ae3ce4d42e639a971c
Your Name <your email>
2023-01-06 14:17:16 -0800

    argument 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ * @argc: counter of arguments
+ * @argv: arguments vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, add = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b]; b++)
+		{
+			if (argv[a][b] < '0' || argv[a][b] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[a]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
+
