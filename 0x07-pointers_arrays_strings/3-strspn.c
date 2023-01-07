1897ef9bb8afe9f6d0462da5868f1b0a4ea8027a
Your Name <your email>
2023-01-07 09:32:20 -0800

    more 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of prefix substring
+ * @s: string
+ * @accept: bytes to be checked
+ * Return: unsigned int
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a = 0;
+	int b;
+	int c;
+
+	while (*s != '\0')
+	{
+		c = 0;
+		while (*(accept + b) != '\0')
+		{
+			if (*s == *(accept + b))
+				c = 1;
+			b++;
+		}
+		b = 0;
+		if (c == 0)
+			break;
+		a++;
+		s++;
+	}
+	return (a);
+}
