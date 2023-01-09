eecc516c21e226d13bf148136153c9899419b3d0
NewGit1999 <yasminebenali32@gmail.com>
2023-01-09 08:43:08 -0800

    malloc 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 	int **arr;
 	int i, j;
 
-	if (width <= o || height <= 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 	arr = malloc(sizeof(int *) * height);
 	if (arr == NULL)
