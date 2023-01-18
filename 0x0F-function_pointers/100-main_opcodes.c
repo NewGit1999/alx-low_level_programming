4a0660713eb77a6a5c119869e9215645ee2c3eaf
NewGit1999 <yasminebenali32@gmail.com>
2023-01-18 07:48:47 -0800

    task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints opcode
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int bt, i;
+	char *arr;
+
+	if (arg != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bt = atoi(argv[1]);
+	if (bt < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	arr = (char *)main;
+	for (i = 0; i < bt; i++)
+	{
+		if (i == bt - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
+}
