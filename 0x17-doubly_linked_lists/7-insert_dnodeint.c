e793015457968420207d95dae87e8b49e933e779
NewGit1999 <yasminebenali32@gmail.com>
2023-03-16 15:47:44 -0700

    insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,7 +8,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *a = *h, node;
+	dlistint_t *a = *h, *node;
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
