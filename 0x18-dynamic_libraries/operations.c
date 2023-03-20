1868e3d4bf9aabd49f2a245190d106c9608117fa
NewGit1999 <yasminebenali32@gmail.com>
2023-03-20 04:48:42 -0700

    add sub div mod mul

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,59 @@
+/**
+ * add - Addition
+ * @a: number 1
+ * @b: number 2
+ * Return: The sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substraction
+ * @a: number 1
+ * @b: number 2
+ * Return: sub of a and b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplication
+ * @a: number 1
+ * @b: number 2
+ * Return: multip of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * mod - modulo
+ * @a: number 1
+ * @b: number 2
+ * Return: modulo of a and b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * div - division
+ * @a: number 1
+ * @b: number 2
+ * Return: division of a and b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
