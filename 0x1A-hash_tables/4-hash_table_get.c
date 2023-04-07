da8796a9696d9ce9770b0cbeefa0fe1965496189
NewGit1999 <yasminebenali32@gmail.com>
2023-04-07 09:12:23 -0700

    retrv value of key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retirves a value of key
+ * @ht: hash table
+ * @key: key to look for
+ * Return: value associated
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int a;
+	hash_node_t *b;
+
+	if (ht == NULL || strlen(key) == 0)
+		return (NULL);
+	a = key_index(const unsigned char *)key, ht->size;
+	b = ht->array[a];
+	if (b == NULL)
+		return (NULL);
+
+	while (b != NULL)
+	{
+		if (strcmp(key, b->key) == 0)
+		{
+			return (b->value);
+		}
+		b = b->next;
+	}
+	return (NULL);
+}
