385ce42cda2d2dbce8159635e213161e5099adf1
NewGit1999 <yasminebenali32@gmail.com>
2023-04-07 09:28:04 -0700

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints hash table
+ * @ht: hash table
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *tab;
+	unsigned long int a;
+	int print = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	a = 0;
+	while (a < ht->size)
+	{
+		if (ht->array[a] != NULL)
+		{
+			tab = ht->array[a];
+			while (tab != NULL)
+			{
+				if (print)
+					printf("' ");
+				printf("'%s': '%s'", tab->key, tab->value);
+				print = 1;
+				tab = tab->next;
+			}
+		}
+		a++;
+	}
+	printf("}\n");
+}
